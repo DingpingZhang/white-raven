5b7b665833d0a172ebb7e235efef5c41b9eb40a9
Dingping Zhang <DingpingZhang@outlook.com>
2021-01-18 01:00:30 +0800

    Save in progress work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import ContactItem from './components/contact-item';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ContactItem
+        messageCount={0}
+        avatar="https://avatars0.githubusercontent.com/u/8541016?s=460&u=7920c831536d48ec046e6bef42393410fda4f14c&v=4"
+        username="Jack Sparrow"
+        lastMessage="Some common custom markup extensions for WPF."
+        lastMessageTimestamp="Last seen 1 day ago"
+      ></ContactItem>
+      <ContactItem
+        selected
+        messageCount={0}
+        avatar="https://avatars0.githubusercontent.com/u/8541016?s=460&u=7920c831536d48ec046e6bef42393410fda4f14c&v=4"
+        username="Jack Sparrow"
+        lastMessage="Some common custom markup extensions for WPF."
+        lastMessageTimestamp="Last seen 1 day ago"
+      ></ContactItem>
+      <ContactItem
+        messageCount={0}
+        avatar="https://avatars0.githubusercontent.com/u/8541016?s=460&u=7920c831536d48ec046e6bef42393410fda4f14c&v=4"
+        username="Jack Sparrow"
+        lastMessage="Some common custom markup extensions for WPF."
+        lastMessageTimestamp="Last seen 1 day ago"
+      ></ContactItem>
+      <ContactItem
+        messageCount={0}
+        avatar="https://avatars0.githubusercontent.com/u/8541016?s=460&u=7920c831536d48ec046e6bef42393410fda4f14c&v=4"
+        username="Jack Sparrow"
+        lastMessage="Some common custom markup extensions for WPF."
+        lastMessageTimestamp="Last seen 1 day ago"
+      ></ContactItem>
+      <ContactItem
+        messageCount={0}
+        avatar="https://avatars0.githubusercontent.com/u/8541016?s=460&u=7920c831536d48ec046e6bef42393410fda4f14c&v=4"
+        username="Jack Sparrow"
+        lastMessage="Some common custom markup extensions for WPF."
+        lastMessageTimestamp="Last seen 1 day ago"
+      ></ContactItem>
     </div>
   );
 }
---
+++ src/components/contact-item.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+import '../styles/contact-item.scss';
+
+type ContactItemProps = {
+  messageCount: number;
+  selected?: boolean;
+  avatar: string;
+  username: string;
+  lastMessage: string;
+  lastMessageTimestamp: string;
+};
+
+export default function ContactItem({
+  messageCount,
+  selected,
+  avatar,
+  username,
+  lastMessage,
+  lastMessageTimestamp,
+}: ContactItemProps) {
+  const contactItemClass = classNames('contact-item', {
+    'has-message': messageCount,
+    selected,
+  });
+
+  return (
+    <div className={contactItemClass}>
+      <span className="red-dot"></span>
+      <img className="avatar" src={avatar} alt="avatar" />
+      <span className="username">{username}</span>
+      {/* TODO: converter to display string */}
+      <span className="last-message-time">{lastMessageTimestamp}</span>
+      <span className="last-message">{lastMessage}</span>
+    </div>
+  );
+}
