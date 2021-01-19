66bce6b8b2559bb5bb92ff2cb1ef73b559319f51
Dingping Zhang <DingpingZhang@outlook.com>
2021-01-19 20:23:36 +0800

    Add icon assets.

--- src/views/messages/message-send-box.tsx
+++ src/views/messages/message-send-box.tsx
@@ -1,14 +1,39 @@
+import { ReactComponent as SendIcon } from '../../images/send.svg';
+import { ReactComponent as MoreVerticalIcon } from '../../images/more-vertical.svg';
+import { ReactComponent as AttachmentIcon } from '../../images/attachment.svg';
+import { ReactComponent as FaceIcon } from '../../images/face.svg';
+
 export default function MessageSendBox() {
   return (
     <div className="message-send-box">
-      <button className="btn-circle secondary btn-upload-file"></button>
+      <button className="btn-circle secondary btn-upload-file">
+        <AttachmentIcon
+          style={{ fill: '#d9ecfc', verticalAlign: 'middle' }}
+          width="16px"
+          height="24px"
+        />
+      </button>
       <div className="text-edit-area">
         <input type="text" className="text-box" placeholder="Write a message..." />
-        <button className="btn-circle default btn-face"></button>
-        <button className="btn-circle default btn-more"></button>
+        <button className="btn-circle default btn-face">
+          <FaceIcon
+            style={{ fill: '#d9ecfc', verticalAlign: 'middle' }}
+            width="20px"
+            height="24px"
+          />
+        </button>
+        <button className="btn-circle default btn-more">
+          <MoreVerticalIcon
+            style={{ fill: '#d9ecfc', verticalAlign: 'middle' }}
+            width="20px"
+            height="24px"
+          />
+        </button>
       </div>
       <div className="split-line vertical"></div>
-      <button className="btn-circle primary btn-send"></button>
+      <button className="btn-circle primary btn-send">
+        <SendIcon style={{ fill: '#d9ecfc', verticalAlign: 'middle' }} width="28px" height="28px" />
+      </button>
     </div>
   );
 }
