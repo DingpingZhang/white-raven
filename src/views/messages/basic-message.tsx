77d1a2726f7915267f6b9691a863a698a80622a0
Dingping Zhang <DingpingZhang@outlook.com>
2021-01-20 22:55:04 +0800

    Add 'at', 'image' and 'face' message demo.

--- src/views/messages/basic-message.tsx
+++ src/views/messages/basic-message.tsx
@@ -14,12 +14,39 @@ export default function BasicMessage({ avatar, message, timestamp, highlight }: 
     <div className="basic-message">
       <img className="avatar" src={avatar} alt="avatar" />
       <div className={messageBoxClass}>
-        <div className="message-content">
-          {message.map((item) => (item.type === 'text' ? item.data.text : null))}
-        </div>
+        <div className="message-content">{message.map((item) => convertToHtmlElement(item))}</div>
       </div>
       {/* TODO: convert timestamp to display time text. */}
       <span className="timestamp">{new Date().toDateString()}</span>
     </div>
   );
+}
+
+function convertToHtmlElement(message: Message) {
+  switch (message.type) {
+    case 'text':
+      return <span className="msg-segment msg-type-text">{message.data.text}</span>;
+    case 'at':
+      return <span className="msg-segment msg-type-at">@{message.data.qq} </span>;
+    case 'face':
+      const imageSource = require(`../../images/face/${message.data.id}.gif`);
+      return (
+        <img
+          className="msg-segment msg-type-face"
+          src={imageSource.default}
+          alt={`[CQ:face,id=${message.data.id}]`}
+        />
+      );
+    case 'image':
+      return (
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
+        <img
+          className="msg-segment msg-type-image"
+          src={message.data.file}
+          alt={`[CQ:image,file=${message.data.file}]`}
+        />
+      );
+    default:
+      return null;
+  }
 }
