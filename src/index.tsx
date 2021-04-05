9e324f289ebdb03856195829b1a704b398276f94
Dingping Zhang <DingpingZhang@outlook.com>
2021-04-06 00:24:48 +0800

    Feature: Initial commit for adapting PWA.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { SwitchHost } from 'components/switch-host';
 import { GlobalContextRoot } from 'models/context-components';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './styles/normalize.css';
 import './styles/index.scss';
@@ -21,6 +22,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
